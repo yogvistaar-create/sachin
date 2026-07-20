ab5ec3d07363b1b7e0817640cc5599493065e2d0
yogvistaar-create <yogvistaar@gmail.com>
2026-07-20 08:06:23 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Yog Vistaar Website Loaded");
+
+// ==========================
+// MOBILE MENU
+// ==========================
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
