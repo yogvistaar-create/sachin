7e6d97cdb4479df89062547d883bfcabd6f86f8d
yogvistaar-create <yogvistaar@gmail.com>
2026-07-20 08:48:27 +0530

    commit

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ const navbar = document.querySelector(".navbar");
 menuToggle.addEventListener("click", () => {
     navbar.classList.toggle("active");
 });
+//==============================
+// Navbar Scroll Effect
+//==============================
+
+window.addEventListener("scroll",()=>{
+
+const header=document.querySelector(".header");
+
+header.classList.toggle("scrolled",window.scrollY>40);
+
+});
